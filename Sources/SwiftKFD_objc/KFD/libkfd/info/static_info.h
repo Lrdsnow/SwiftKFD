9d9e79c04dc1fc1a312827a38bcff0b37c14c3cd
Lrdsnow <63671193+Lrdsnow@users.noreply.github.com>
2024-07-13 05:25:07 -0700

    Update static_info.h

--- Sources/SwiftKFD_objc/KFD/libkfd/info/static_info.h
+++ Sources/SwiftKFD_objc/KFD/libkfd/info/static_info.h
@@ -557,7 +557,7 @@ struct kqworkloop {
  * bsd/kern/posix_sem.c
  */
 
-#if !TARGET_OS_MAC || TARGET_OS_SIMULATOR
+#if !TARGET_OS_OSX && !TARGET_OS_MACCATALYST && !TARGET_OS_UIKITFORMAC
 struct pseminfo {
     u32 psem_flags;
     u32 psem_usecount;
